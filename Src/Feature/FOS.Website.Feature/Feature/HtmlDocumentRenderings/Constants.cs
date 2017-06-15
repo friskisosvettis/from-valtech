f006cc8797f481d43c62fdbded5864ed7f672c50
fredrik.hojdegard <fredrik.hojdegard@CLT-6376Q4M>
2017-06-15 13:47:17 +0200

    TG-312 - No Index or Crawling for pages not migrated

---
+++ Src/Feature/FOS.Website.Feature/Feature/HtmlDocumentRenderings/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FOS.Website.Feature.HtmlDocumentRenderings
+{
+    public class Constants
+    {
+        public struct Views
+        {
+            public struct Paths
+            {
+                public const string NoRobotsMetaHeader = "/Feature/HtmlDocumentRenderings/Views/NoRobotsMetaHeader.cshtml";
+            }
+        }
+    }
+}
---
+++ Src/Feature/FOS.Website.Feature/Feature/HtmlDocumentRenderings/Controllers/NoRobotsMetaHeader.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Synthesis;
+using FOS.Website.Feature.HtmlDocumentRenderings;
+using Sitecore.Buckets.Extensions;
+using Valtech.Foundation.SitecoreExtensions;
+using Valtech.Foundation.Synthesis;
+using Sitecore.Data.Items;
+using System.Diagnostics;
+using FOS.Website.Feature.Content.Data;
+
+namespace FOS.Website.Feature.HtmlDocumentRenderings.Controllers
+{
+    public class NoRobotsMetaHeaderController : Controller
+    {
+        public ActionResult HandleMetaDataRobots()
+        {
+            var associationMigratedCheckItem = Sitecore.Context.Item.ClosestAscendantItemOfType<IAssociationNotMigratedWidgetItem>();
+            if (associationMigratedCheckItem != null && !associationMigratedCheckItem.AssociationReady.Value)
+            {
+                return View(Constants.Views.Paths.NoRobotsMetaHeader, null);
+            }
+
+            return new EmptyResult();
+        }
+    }
+}

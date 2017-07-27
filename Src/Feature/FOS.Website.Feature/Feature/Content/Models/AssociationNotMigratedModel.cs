84409ff7b1bab5b184af19d35452b668e44438d4
fhojdegard <fredrik.hojdegard@valtech.se>
2017-07-27 14:36:39 +0200

    TG-507 - Dynamisk go to button label for NotMigrated button.

--- Src/Feature/FOS.Website.Feature/Feature/Content/Models/AssociationNotMigratedModel.cs
+++ Src/Feature/FOS.Website.Feature/Feature/Content/Models/AssociationNotMigratedModel.cs
@@ -6,6 +6,7 @@ using FOS.Website.Feature.Content.Data;
 using Sitecore.Buckets.Extensions;
 using Sitecore.Data.Items;
 using Synthesis;
+using Valtech.Foundation.Dictionary;
 using Valtech.Foundation.Synthesis;
 
 namespace FOS.Website.Feature.Content.Models
@@ -14,5 +15,20 @@ namespace FOS.Website.Feature.Content.Models
     {
         public IAssociationNotMigratedInfoItem GeneralData { get; set; }
         public IAssociationNotMigratedWidgetItem AssociationCheckWidget{ get; set; }
+        public string label_Button
+        {
+            get
+            {
+                var label = DictionaryRepository.Default.GetRawValueText("/Content/AssociationNotMigrated",
+                    "LabelGoToButton", "Go to Friskis");
+
+                if (AssociationCheckWidget != null && label.Contains("{name}"))
+                {
+                    label = label.Replace("{name}", AssociationCheckWidget.InnerItem.DisplayName);
+                }
+
+                return label;
+            }
+        }
     }
 }

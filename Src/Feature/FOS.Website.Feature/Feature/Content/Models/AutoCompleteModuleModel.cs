827ea619d890b20590eae64deb4d7ab6ca75e0ae
fhojdegard <fredrik.hojdegard@valtech.se>
2017-08-21 16:30:44 +0200

    TG-644 - Added directory for hardcoded labels for Autocomplete

--- Src/Feature/FOS.Website.Feature/Feature/Content/Models/AutoCompleteModuleModel.cs
+++ Src/Feature/FOS.Website.Feature/Feature/Content/Models/AutoCompleteModuleModel.cs
@@ -16,6 +16,11 @@ namespace FOS.Website.Feature.Content.Models
     {
         public IAutoCompleteModuleItem AutoCompleteModuleItem { get; set; }
         public IHtmlString label_InputFieldBackground { get; } = new HtmlString(DictionaryRepository.Default.GetRawValueText("/Content/AutoCompleteModule", "InputFieldBackground", "Find an area, association or training center"));
+        public IHtmlString label_NoMatch = new HtmlString(DictionaryRepository.Default.GetRawValueText("/Content/AutoCompleteModule", "NoMatch", "Vi kan inte hitta det du letar efter tyvärr!"));
+        public IHtmlString label_Area = new HtmlString(DictionaryRepository.Default.GetRawValueText("/Content/AutoCompleteModule", "Area", "Område"));
+        public IHtmlString label_TrainingcenterOne = new HtmlString(DictionaryRepository.Default.GetRawValueText("/Content/AutoCompleteModule", "TrainingcenterOne", "träningsställe"));
+        public IHtmlString label_TrainingcenterMore = new HtmlString(DictionaryRepository.Default.GetRawValueText("/Content/AutoCompleteModule", "TrainingcenterMore", "träningsställen"));
+        public IHtmlString label_ShowAll = new HtmlString(DictionaryRepository.Default.GetRawValueText("/Content/AutoCompleteModule", "ShowAll", "Visa alla"));
 
         public AutoCompleteModuleModel()
         {

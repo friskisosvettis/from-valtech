d369a57338b5885e886cead877ceb79c072ca39a
DK\villads.ruby <villadsruby@gmail.com>
2017-08-23 11:45:39 +0200

    TG-663 Fixing localization of date time properties based on the site content language culture info

---
+++ Src/Foundation/Valtech.Foundation/SitecoreExtensions/DateExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Synthesis.FieldTypes.Interfaces;
+
+namespace Valtech.Foundation.SitecoreExtensions
+{
+    public static class DateExtensions
+    {
+        public static string ToLocalizedString(this IDateTimeField dateTimeField, string dateTimeFormat)
+        {
+            if (!dateTimeField.HasValue)
+            {
+                return string.Empty;
+            }
+
+            return dateTimeField.Value.ToString(dateTimeFormat, Sitecore.Context.Site.ContentLanguage.CultureInfo);
+        }
+    }
+}

9fe2d22285b0bd35b3d76310fb26c2129bec9a7c
fhojdegard <fredrik.hojdegard@valtech.se>
2017-08-28 15:09:51 +0200

    - Removed Unused Placeholders and Renderings

--- Src/Project/FOS.Website.Project/Project/Friskis/Constants.cs
+++ Src/Project/FOS.Website.Project/Project/Friskis/Constants.cs
@@ -18,16 +18,7 @@ namespace FOS.Website.Project.Friskis
                 public const string HeroPage = "/Project/Friskis/Views/HeroPage.cshtml";
                 public const string TrainingCenterPage = "/Project/Friskis/Views/TrainingCenterPage.cshtml";
 
-                // Column Placeholder layouts
-                public const string Layout_full_width = "/Project/Friskis/Views/ContentRow/Layout_full_width.cshtml";
-                public const string Layout_half = "/Project/Friskis/Views/ContentRow/Layout_half.cshtml";
-                public const string Layout_one = "/Project/Friskis/Views/ContentRow/Layout_one.cshtml";
-                public const string Layout_one_two = "/Project/Friskis/Views/ContentRow/Layout_one_two.cshtml";
-                public const string Layout_thirds = "/Project/Friskis/Views/ContentRow/Layout_thirds.cshtml";
-                public const string Layout_two_one = "/Project/Friskis/Views/ContentRow/Layout_two_one.cshtml";
-
                 // Static content row. Will not have placeholde settings and will not me editable from Sitecore
-                public const string Static_full_width = "/Project/Friskis/Views/StaticContentRow/Static_full_width.cshtml";
                 public const string Static_one = "/Project/Friskis/Views/StaticContentRow/Static_one.cshtml";
             }
         }
--- Src/Project/FOS.Website.Project/Project/Friskis/Controllers/StaticRowPlaceholderController.cs
+++ Src/Project/FOS.Website.Project/Project/Friskis/Controllers/StaticRowPlaceholderController.cs
@@ -8,11 +8,6 @@ namespace FOS.Website.Project.Friskis.Controllers
 {
     public class StaticRowPlaceholderController : Controller
     {
-        public ActionResult GetStaticFullWidthView()
-        {
-            return View(Constants.Views.Paths.Static_full_width);
-        }
-
         public ActionResult GetStaticOneView()
         {
             return View(Constants.Views.Paths.Static_one);
